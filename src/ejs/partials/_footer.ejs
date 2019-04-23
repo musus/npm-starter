98dffa230886ceb48696dbd1bb23bbc28627ac24
Susumu Seino <seino@susu.mu>
2019-04-24 01:17:03 +0900

    some fix

---
+++ src/ejs/partials/_footer.ejs
@@ -0,0 +1,3 @@
+<footer class="footer">
+    copyright
+</footer>
--- src/js/app.js
+++
@@ -1,6 +0,0 @@
-jQuery(function ($) {
-
-    $(".has-desc").click(function () {
-        $(this).children(".work-list__desc").slideToggle();
-    })
-});
